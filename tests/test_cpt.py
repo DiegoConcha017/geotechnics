432b97b424e718a45199c8cab48490bb71c32a6c
Breinbaas | Rob van Putten <breinbaas@gmail.com>
2017-07-25 14:59:44 +0200

    Changed path to data

--- tests/test_cpt.py
+++ tests/test_cpt.py
@@ -1,7 +1,8 @@
 def test_cpt():
     import geotechnics as gt
     cpt = gt.cpt.CPT()
-    cpt.read('data/b_cpt.gef')
+    cpt.read('tests/data/a_cpt.gef')
+    cpt.read('tests/data/b_cpt.gef')
     cpt.plot()
 
 if __name__=="__main__":
